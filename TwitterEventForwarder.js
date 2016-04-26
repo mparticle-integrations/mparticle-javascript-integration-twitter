d828e45ce40d0f018bb6d8576bfdfbb71b63791c
Siva <sivaa@mparticle.com>
2016-04-26 09:56:18 -0400

    Changed forwarder name

--- TwitterEventForwarder.js
+++ TwitterEventForwarder.js
@@ -13,7 +13,7 @@
 //  limitations under the License.
 
 (function (window) {
-    var name = 'TwitterEventForwarder',
+    var name = 'Twitter',
         MessageType = {
             PageView    : 3    
         };
