9c79b80452f326b314cccc2ae7edd478fd77b7a0
Robert Ing <rmi22186@gmail.com>
2023-11-30 17:32:40 -0500

    ci: Add CI/CD and semantic release

---
+++ release.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+    branches: ['master'],
+    tagFormat: 'v${version}',
+    repositoryUrl:
+        'https://github.com/mparticle-integrations/mparticle-javascript-integration-twitter',
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [
+                    { type: 'feat', release: 'minor' },
+                    { type: 'ci', release: 'patch' },
+                    { type: 'fix', release: 'patch' },
+                    { type: 'docs', release: 'patch' },
+                    { type: 'test', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'style', release: 'patch' },
+                    { type: 'build', release: 'patch' },
+                    { type: 'chore', release: 'patch' },
+                    { type: 'revert', release: 'patch' },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'angular',
+            },
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md',
+            },
+        ],
+        ['@semantic-release/npm'],
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd: 'sh ./scripts/release.sh',
+            },
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: [
+                    'dist/TwitterEventForwarder.common.js',
+                    'dist/TwitterEventForwarder.iife.js',
+                ],
+            },
+        ],
+        [
+            '@semantic-release/git',
+            {
+                assets: ['package.json', 'package-lock.json', 'CHANGELOG.md'],
+                message:
+                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+            },
+        ],
+    ],
+};
